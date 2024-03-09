d29b10de098534019a2d0314849ee285a25d313b
NguyenVanDuongTDH <154052803+NguyenVanDuongTDH@users.noreply.github.com>
2024-03-09 16:01:04 +0700

    up

--- lib/android_serial.dart
+++ lib/android_serial.dart
@@ -1,3 +1,4 @@
 export 'package:usb_serial/usb_serial.dart' show UsbPort;
 export 'src/android_usb.dart' show SerialAndroidClientUSB;
 export 'src/android_bluetooth.dart' show SerialAndroidBluetooth;
+export 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
